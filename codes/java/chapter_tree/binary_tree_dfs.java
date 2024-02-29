5bb1efbbea04d1e0de3805408960a4489c59d168
yangzb <1294043509@qq.com>
2024-02-29 10:38:15 +0800

    2024-02-29: 1、binary_tree_dfs Java 实现迭代版的二叉树前序遍历

--- codes/java/chapter_tree/binary_tree_dfs.java
+++ codes/java/chapter_tree/binary_tree_dfs.java
@@ -13,33 +13,94 @@ public class binary_tree_dfs {
     // 初始化列表，用于存储遍历序列
     static ArrayList<Integer> list = new ArrayList<>();
 
-    /* 前序遍历 */
-    static void preOrder(TreeNode root) {
+    /* 前序遍历-递归 */
+    static void preOrderRecursion(TreeNode root) {
         if (root == null)
             return;
         // 访问优先级：根节点 -> 左子树 -> 右子树
         list.add(root.val);
-        preOrder(root.left);
-        preOrder(root.right);
+        preOrderRecursion(root.left);
+        preOrderRecursion(root.right);
     }
 
-    /* 中序遍历 */
-    static void inOrder(TreeNode root) {
+    /* 中序遍历-递归 */
+    static void inOrderRecursion(TreeNode root) {
         if (root == null)
             return;
         // 访问优先级：左子树 -> 根节点 -> 右子树
-        inOrder(root.left);
+        inOrderRecursion(root.left);
         list.add(root.val);
-        inOrder(root.right);
+        inOrderRecursion(root.right);
     }
 
-    /* 后序遍历 */
-    static void postOrder(TreeNode root) {
+    /* 后序遍历-递归 */
+    static void postOrderRecursion(TreeNode root) {
         if (root == null)
             return;
         // 访问优先级：左子树 -> 右子树 -> 根节点
-        postOrder(root.left);
-        postOrder(root.right);
+        postOrderRecursion(root.left);
+        postOrderRecursion(root.right);
+        list.add(root.val);
+    }
+
+    /* 前序遍历-迭代 */
+    static void preOrderIterate(TreeNode root) {
+        Stack<TreeNode> nodeStack = new Stack<>();
+        if (root == null)
+            return;
+        // 访问优先级：根节点 -> 左子树 -> 右子树
+        TreeNode curNode = root;
+       do{
+           //先访问节点的左子树
+           if(null != curNode.left){
+               nodeStack.push(curNode);
+               list.add(curNode.val);
+               curNode = curNode.left;
+           }else{
+               //再访问节点的右子树
+               if(null != curNode.right){
+                   list.add(curNode.val);
+                   curNode = curNode.right;
+                   nodeStack.push(curNode);
+               }else{
+                   //如果当前节点左右子树都没有，这说明是叶子节点。
+                   //需要从栈中去除其父节点。
+                   list.add(curNode.val);
+                   curNode = nodeStack.pop();
+                   //当前节点存在右子树
+                   if(null != curNode.right){
+                       //当栈底的节点是根节点的时候，需要做一次栈底节点
+                       //替换，替换为根节点下，右子树的节点。
+                       if(curNode == root){
+                           nodeStack.push(curNode.right);
+                       }
+                       curNode = curNode.right;
+                   }else{
+                       //不存在则弹出其父节点或其本身
+                       curNode = nodeStack.pop();
+                   }
+               }
+           }
+       }while (!nodeStack.isEmpty());
+    }
+
+    /* 中序遍历-迭代 */
+    static void inOrderIterate(TreeNode root) {
+        if (root == null)
+            return;
+        // 访问优先级：左子树 -> 根节点 -> 右子树
+        inOrderIterate(root.left);
+        list.add(root.val);
+        inOrderIterate(root.right);
+    }
+
+    /* 后序遍历-迭代 */
+    static void postOrderIterate(TreeNode root) {
+        if (root == null)
+            return;
+        // 访问优先级：左子树 -> 右子树 -> 根节点
+        postOrderIterate(root.left);
+        postOrderIterate(root.right);
         list.add(root.val);
     }
 
@@ -50,19 +111,24 @@ public class binary_tree_dfs {
         System.out.println("\n初始化二叉树\n");
         PrintUtil.printTree(root);
 
-        /* 前序遍历 */
+        /* 前序遍历-递归 */
         list.clear();
-        preOrder(root);
+        preOrderRecursion(root);
         System.out.println("\n前序遍历的节点打印序列 = " + list);
 
-        /* 中序遍历 */
+        /* 中序遍历-递归 */
         list.clear();
-        inOrder(root);
+        inOrderRecursion(root);
         System.out.println("\n中序遍历的节点打印序列 = " + list);
 
-        /* 后序遍历 */
+        /* 后序遍历-递归 */
         list.clear();
-        postOrder(root);
+        postOrderRecursion(root);
         System.out.println("\n后序遍历的节点打印序列 = " + list);
+
+        /* 前序遍历-递归 */
+        list.clear();
+        preOrderIterate(root);
+        System.out.println("\n前序遍历的节点打印序列 = " + list);
     }
 }
